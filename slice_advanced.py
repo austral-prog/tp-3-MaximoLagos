86d17668d63300f7b4feeb336bfbc630a5aeb609
MaximoLagos <mlagosmarmol1@mail.austral.edu.ar>
2025-04-11 12:08:12 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,7 +1,4 @@
 def slice_advanced():
-	texto = input("Ingresa texto:")
-
-	print(texto[4: :2])
-
-slice_advanced()
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+	texto = input("Ingresa un texto: ")
+	resultado = texto [4::2]
+	print(resultado)
